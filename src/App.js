eeecdeaecf923e57d184d782d920d0f908a0a5f1
Alhassan Abdul-Razak <alhassan.razak@amalitech.com>
2022-06-14 08:00:06 +0000

    adding footer and media content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from './layouts/Layout';
+import ImageSlider from './components/Carousel/ImageSlider';
+
 
 function App() {
   return (
     <div className="App">
       <Layout />
+      <ImageSlider/>
     </div>
   );
 }
