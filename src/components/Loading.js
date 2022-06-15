a8c36a57b408b444e0f8cdc915dab6cbd7693639
Alhassan Abdul-Razak <alhassan.razak@amalitech.com>
2022-06-15 13:38:00 +0000

    add loading to display page

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,9 @@
-import React from 'react';
-
-class Loading extends React.Component {
-
-    render() {
-        return (
-            <div className="spinner-border" role="status">
-                <span className="sr-only">Loading </span>
-            </div>
-        )
-    }
-
-}
-export default Loading;
+const Loading = () => {
+    return (
+      <div className="spinner-border" role="status">
+                   <span className="sr-only">Loading </span>
+               </div>
+    )
+  }
+  
+  export default Loading;
